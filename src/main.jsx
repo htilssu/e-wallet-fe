b159cc72b56a8d7076c66879a4afb288bddafbc5
htilssu <tolashuu@gmail.com>
2024-06-27 12:16:31 +0700

    feat: install react-buddy

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AuthProvider } from './context/AuthContext.jsx'
+import { DevSupport } from '@react-buddy/ide-toolbox'
+import { ComponentPreviews, useInitial } from './dev/index.js'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+        <React.StrictMode>
+            <AuthProvider>
+                <DevSupport ComponentPreviews={ComponentPreviews}
+                            useInitialHook={useInitial}
+                >
+                    <App/>
+                </DevSupport>
+            </AuthProvider>
+        </React.StrictMode>,
 )
