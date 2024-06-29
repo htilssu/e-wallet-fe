ff27379022079a72e40125543aef8f9c03055b4d
Tuan980Blue <tuanmeo980provip@gmail.com>
2024-06-29 22:24:07 +0700

    Login Tuan

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Home from "./components/topup/test.jsx";
 
 
 function App() {
 
   return (
     <>
+      <div>
+          <Home></Home>
+          <TestComponent/>
+      </div>
     </>
   )
 }
