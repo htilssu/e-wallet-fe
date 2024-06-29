f981cb0324192fb34a84675425983562751a0272
Tuan980Blue <166797678+Tuan980Blue@users.noreply.github.com>
2024-06-29 22:39:08 +0700

    Login Tuan (#5)
    Co-authored-by: Hisu <81826098+htilssu@users.noreply.github.com>
    Co-authored-by: TuanModel <166797678+Tuan980Blue@users.noreply.github.com>

---
+++ src/components/auth/LoginForm.jsx
@@ -0,0 +1,28 @@
+
+// cach tao ra: rafce
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+import './LoginRegister.css';  //add css
+import LoginComponent from './LoginComponent.jsx';
+import RegisterComponent from "./RegisterComponent.jsx";
+
+const LoginRegister = () => {
+
+    const [action, setAction] = useState('');  //khai báo một biến action và một hàm setAction để cập nhật giá trị của biến này.
+
+    const registerLink = () => {
+        setAction(' active');
+    };
+    const loginLink = () => {
+        setAction('');
+    };
+
+    return (
+        <div className = {`wrapper${action}`}>
+            <LoginComponent registerLink={registerLink}></LoginComponent>
+            <RegisterComponent loginLink={loginLink}></RegisterComponent>
+        </div>
+    )
+}
+
+export default LoginRegister
