45998534f868e1be93899dc1e45595fc50279760
Tuan980Blue <166797678+Tuan980Blue@users.noreply.github.com>
2024-06-30 00:43:49 +0700

    Start code fontend (#6)
    * Login Tuan

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import './App.css'
-import Home from "./components/topup/test.jsx";
-
+import LoginForm from "./components/auth/LoginForm.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import InforPopup from "./components/topup/InforPopup.jsx";
 
 function App() {
-
-  return (
-    <>
-      <div>
-          <Home></Home>
-          <TestComponent/>
-      </div>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    <Route path="/login" element={<LoginForm />} />
+                    <Route path="/home" element={<InforPopup />} />
+                    <Route path="/" element={<LoginForm />} /> {/* Route mặc định */}
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
