b141ac1be60c5072a4b4a23d61ed0b9d2fd59a51
htilssu <tolashuu@gmail.com>
2024-07-01 00:48:37 +0700

    feat: Add atm/link route

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import './App.css'
-import LoginForm from "./components/auth/LoginForm.jsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Footer from "./components/footer/Footer.jsx";
+import './App.css';
+import LoginForm from './components/auth/LoginForm.jsx';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Footer from './components/footer/Footer.jsx';
 
 function App() {
-    return (
-        <BrowserRouter>
-            <div className="App">
-                <Routes>
-                    <Route path="/login" element={<LoginForm />} />
-                    <Route path="/home" element={<Footer />} />
-                    <Route path="/footer" element={<Footer />} />
-                    <Route path="/" element={<LoginForm />} /> {/* Route mặc định */}
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
+  return (
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/login" element={<LoginForm/>}/>
+            <Route path="/home" element={<Footer/>}/>
+            <Route path="/atm/link" element={<Footer/>}/>
+            <Route path="/footer" element={<Footer/>}/>
+            <Route path="/" element={<LoginForm/>}/> {/* Route mặc định */}
+          </Routes>
+        </div>
+      </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
