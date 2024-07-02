ad5dffa9c731b425d3a5a3d0564b12c2b24c1e92
htilssu <tolashuu@gmail.com>
2024-07-03 01:02:42 +0700

    feat: remove unused exposes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
-import federation from '@originjs/vite-plugin-federation';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: 'remote-app',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './Button': './src/Button.vue',
-      },
-      shared: ['react', 'react-dom'],
-    })],
+      name: "remote-app",
+      filename: "remoteEntry.js",
+      exposes: {},
+      shared: ["react", "react-dom"],
+    }),
+  ],
 });
