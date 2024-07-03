d8e89cb41498ad9aee7bbb7feae154cf94948ac2
htilssu <tolashuu@gmail.com>
2024-07-04 01:24:28 +0700

    hotfix: duplicate footer

--- src/components/topup/TopUp.jsx
+++ src/components/topup/TopUp.jsx
@@ -1,31 +1,30 @@
-import {lazy, Suspense} from "react";
+import { lazy, Suspense } from "react";
 
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const InfoTopUp = lazy(() => {
-    return delay(100).then(() => import('./InfoPopup.jsx'));
+  return delay(100).then(() => import("./InfoPopup.jsx"));
 });
 import Footer from "../footer/Footer.jsx";
-import {MyWallet} from "../infoAccount/PersonalInfoForm.jsx";
+import { MyWallet } from "../infoAccount/PersonalInfoForm.jsx";
 
 const TopUp = () => {
-    return (
-        <div className="min-h-screen flex flex-col">
-            <div className="flex justify-center items-center flex-1">
-                <div className="container mx-auto p-6 flex flex-col md:flex-row">
-                    <div className="flex justify-end w-full md:w-2/3 ml-auto">
-                        <Suspense fallback={<h1>Tuan</h1>}>
-                           <InfoTopUp/>
-                       </Suspense>
-                    </div>
-                    <div className="order-first md:order-last w-full md:w-1/3 mb-6 md:mb-0">
-                        <MyWallet/>
-                    </div>
-                </div>
-            </div>
-            <Footer/>
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="flex justify-center items-center flex-1">
+        <div className="container mx-auto p-6 flex flex-col md:flex-row">
+          <div className="flex justify-end w-full md:w-2/3 ml-auto">
+            <Suspense fallback={<h1>Tuan</h1>}>
+              <InfoTopUp />
+            </Suspense>
+          </div>
+          <div className="order-first md:order-last w-full md:w-1/3 mb-6 md:mb-0">
+            <MyWallet />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default TopUp
+export default TopUp;
