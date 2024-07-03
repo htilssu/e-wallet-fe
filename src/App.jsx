57ec7b400dd9f7c11a7280b335d7cd514a19bb7a
htilssu <tolashuu@gmail.com>
2024-07-03 10:53:07 +0700

    feat: split bundle react

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import LoginForm from "./components/auth/LoginForm.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer.jsx";
 import PersonalInfoForm from "./components/infoAccount/PersonalInfoForm.jsx";
-import Topup from "./components/topup/Topup.jsx";
+import TopUp from "./components/topup/Topup.jsx";
 
 function App() {
   return (
@@ -13,8 +13,7 @@ function App() {
             <Route path="/login" element={<LoginForm />} />
             <Route path="/home" element={<PersonalInfoForm />} />
             <Route path="/atm/link" element={<Footer />} />
-            <Route path="/topup" element={<Topup />} />
-            <Route path="/" element={<LoginForm />} />
+            <Route path="/topup" element={<TopUp />} />
           </Routes>
         </div>
       </BrowserRouter>
--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,18 +1,18 @@
-import { createContext, useContext, useState } from 'react'
-import PropTypes from 'prop-types'
+import { createContext, useContext, useState } from "react";
+import PropTypes from "prop-types";
 
+const Context = createContext({});
+export const useAuth = () => useContext(Context);
+export const AuthProvider = (props) => {
+  const [user, setUser] = useState(null);
 
-const Context = createContext({})
-export const useAuth = () => useContext(Context)
-export const AuthProvider = props => {
-    const [user, setUser] = useState(null)
-    return (
-            <Context.Provider value={{ user, setUser }}>
-                {props.children}
-            </Context.Provider>
-    )
-}
+  return (
+    <Context.Provider value={{ user, setUser }}>
+      {props.children}
+    </Context.Provider>
+  );
+};
 
 AuthProvider.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
