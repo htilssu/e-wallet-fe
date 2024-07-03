adb36de9174ed9f40a5d406d7db6135f96b59564
toand <toandong2004@gmail.com>
2024-07-03 20:54:06 +0700

    themlienket

--- src/components/add_atm_linked/AddInfoAtm.jsx
+++ src/components/add_atm_linked/AddInfoAtm.jsx
@@ -1,5 +1,9 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
+
+const fetchBankList = () => {
+    return axios.get('https://api.vietqr.io/v2/banks');
+};
 
 const AddInfoAtm = () => {
     const [selectedBank, setSelectedBank] = useState('');
@@ -15,16 +19,13 @@ const AddInfoAtm = () => {
     const [successMessage, setSuccessMessage] = useState('');
 
     useEffect(() => {
-        const fetchBankList = async () => {
-            try {
-                const response = await axios.get('https://api.vietqr.io/v2/banks');
-                setBankList(response.data.data);
-            } catch (error) {
+        fetchBankList()
+            .then(result => {
+                setBankList(result.data.data);
+            })
+            .catch(error => {
                 console.error('Error fetching bank list:', error);
-            }
-        };
-
-        fetchBankList();
+            });
     }, []);
 
     const selectBank = (shortName) => {
@@ -43,7 +44,7 @@ const AddInfoAtm = () => {
 
         const currentDate = new Date();
         const currentYear = currentDate.getFullYear();
-        const currentMonth = currentDate.getMonth() + 1; // GetMonth returns 0-indexed month
+        const currentMonth = currentDate.getMonth() + 1;
 
         const [month, year] = date.split('/').map(Number);
         if (month < 1 || month > 12) {
@@ -70,20 +71,16 @@ const AddInfoAtm = () => {
 
     const handleDateChange = (e) => {
         let value = e.target.value;
-        // Chèn dấu gạch chéo tự động sau khi nhập tháng
         if (value.length === 2 && !value.includes('/')) {
             value = value + '/';
         }
-        // Giới hạn độ dài của giá trị nhập vào tối đa 7 ký tự (mm/yyyy)
         if (value.length > 7) {
             value = value.slice(0, 7);
         }
 
-        // Lưu trữ giá trị nhập trước đó
         setPreviousNgayphathanh(ngayphathanh);
         setNgayphathanh(value);
 
-        // Kiểm tra hợp lệ và hiển thị lỗi nếu cần
         if (value.length === 7 || value.length === 2) {
             validateDate(value);
         } else {
@@ -124,122 +121,118 @@ const AddInfoAtm = () => {
 
     return (
         <div className={"md:p-6 flex items-center justify-center"}>
-        <div className="Add-app flex flex-wrap justify-center items-center max-w-[1200px] w-full bg-white border-[1px] mt-5 p-8 rounded-2xl">
-            <div className="w-full md:flex " >
-                {/* bên trái */}
-                <div className="w-full md:w-1/2 justify-center p-4 bg-gray-100 border border-gray-300 rounded-2xl mb-5 md:mb-0 h-full">
-                    <div className="mb-4">
-                        <p className="text-lg">{accountName}</p>
-                    </div>
-                    <div className="mb-4">
-                        <p className="stk font-medium text-gray-700">Số tài khoản:</p>
-                        <p className="text-lg">{accountNumber}</p>
-                    </div>
-                    <div className="mb-5">
-                        <p className="nganhang font-medium text-gray-700">Ngân hàng:</p>
-                        <p className="text-lg">{selectedBank}</p>
-                    </div>
-                    <div className="mb-5">
-                        <p className="font-medium text-gray-700">Ngày phát hành:</p>
-                        <p className="text-lg">{ngayphathanh}</p>
-                    </div>
-                </div>
-
-                {/* bên phải */}
-                <div className="w-full md:w-2/3 p-4">
-                    <div className="flex flex-col gap-4">
-                        <div className="flex items-center">
-                            <span className="w-1/4 text-right mr-4 text-md">Chọn ngân hàng:</span>
-                            <select
-                                className="w-3/4 p-2 border border-gray-300 focus:outline-none"
-                                value={selectedBank}
-                                onChange={(e) => selectBank(e.target.value)}
-                            >
-                                <option value="" disabled selected hidden>Chọn ngân hàng</option>
-                                {bankList.map((bank, index) => (
-                                    <option key={index} value={bank.shortName}>
-                                        {bank.shortName}
-                                    </option>
-                                ))}
-                            </select>
+            <div className="Add-app flex flex-wrap justify-center items-center max-w-[1200px] w-full bg-white border-[1px] mt-5 p-8 rounded-2xl">
+                <div className="w-full md:flex " >
+                    <div className="w-full md:w-1/2 justify-center p-4 bg-gray-100 border border-gray-300 rounded-2xl mb-5 md:mb-0 h-full">
+                        <div className="mb-4">
+                            <p className="text-lg">{accountName}</p>
                         </div>
-
-                        <div className="flex items-center">
-                            <span className="w-1/4 text-right mr-4 text-md">Tên tài khoản:</span>
-                            <input
-                                className="w-3/4 p-2 border border-gray-300 bg-gray-300 text-gray-700 focus:outline-none"
-                                value={accountName}
-                                readOnly
-                            />
+                        <div className="mb-4">
+                            <p className="stk font-medium text-gray-700">Số tài khoản:</p>
+                            <p className="text-lg">{accountNumber}</p>
                         </div>
-                        <div className="flex items-center flex-col">
-                            <div className="flex  w-full mb-4 items-baseline">
-                                <div className="w-1/4 text-right mr-4 text-md items-baseline ">
-                                    <span>Số thẻ:</span>
-                                </div>
-                                <div className="w-3/4">
-                                    <div className="relative">
-                                        <input
-                                            className="w-full p-2 border border-gray-300 focus:outline-none align-baseline"
-                                            type="text"
-                                            value={accountNumber}
-                                            onChange={(e) => setAccountNumber(e.target.value)}
-                                        />
-                                        {errors.accountNumber && (
-                                            <p className="text-red-500 mt-1 left-0">{errors.accountNumber}</p>
-                                        )}
+                        <div className="mb-5">
+                            <p className="nganhang font-medium text-gray-700">Ngân hàng:</p>
+                            <p className="text-lg">{selectedBank}</p>
+                        </div>
+                        <div className="mb-5">
+                            <p className="font-medium text-gray-700">Ngày phát hành:</p>
+                            <p className="text-lg">{ngayphathanh}</p>
+                        </div>
+                    </div>
+
+                    <div className="w-full md:w-2/3 p-4">
+                        <div className="flex flex-col gap-4">
+                            <div className="flex items-center">
+                                <span className="w-1/4 text-right mr-4 text-md">Chọn ngân hàng:</span>
+                                <select
+                                    className="w-3/4 p-2 border border-gray-300 focus:outline-none"
+                                    value={selectedBank}
+                                    onChange={(e) => selectBank(e.target.value)}
+                                >
+                                    <option value="" disabled selected hidden>Chọn ngân hàng</option>
+                                    {bankList.map((bank, index) => (
+                                        <option key={index} value={bank.shortName}>
+                                            {bank.shortName}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+
+                            <div className="flex items-center">
+                                <span className="w-1/4 text-right mr-4 text-md">Tên tài khoản:</span>
+                                <input
+                                    className="w-3/4 p-2 border border-gray-300 bg-gray-300 text-gray-700 focus:outline-none"
+                                    value={accountName}
+                                    readOnly
+                                />
+                            </div>
+                            <div className="flex items-center flex-col">
+                                <div className="flex  w-full mb-4 items-baseline">
+                                    <div className="w-1/4 text-right mr-4 text-md items-baseline ">
+                                        <span>Số thẻ:</span>
+                                    </div>
+                                    <div className="w-3/4">
+                                        <div className="relative">
+                                            <input
+                                                className="w-full p-2 border border-gray-300 focus:outline-none align-baseline"
+                                                type="text"
+                                                value={accountNumber}
+                                                onChange={(e) => setAccountNumber(e.target.value)}
+                                            />
+                                            {errors.accountNumber && (
+                                                <p className="text-red-500 mt-1 left-0">{errors.accountNumber}</p>
+                                            )}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div className="flex items-center flex-col border-b-2 border-gray-300 pb-6">
-                            <div className="flex items-ceter w-full mb-4 items-baseline ">
-                                <div className="w-1/4 textn-right mr-4 text-md ">
-                                    <span>Ngày phát hành thẻ:</span>
-                                </div>
-                                <div className="w-3/4 ">
-                                    <div className="">
-                                        <input
-                                            className="w-full p-2 border border-gray-300 focus:outline-none"
-                                            type="text"
-                                            placeholder="mm/yyyy"
-                                            value={ngayphathanh}
-                                            onChange={handleDateChange}
-                                        />
+                            <div className="flex items-center flex-col border-b-2 border-gray-300 pb-6">
+                                <div className="flex items-ceter w-full mb-4 items-baseline ">
+                                    <div className="w-1/4 textn-right mr-4 text-md ">
+                                        <span>Ngày phát hành thẻ:</span>
+                                    </div>
+                                    <div className="w-3/4 ">
+                                        <div className="">
+                                            <input
+                                                className="w-full p-2 border border-gray-300 focus:outline-none"
+                                                type="text"
+                                                placeholder="mm/yyyy"
+                                                value={ngayphathanh}
+                                                onChange={handleDateChange}
+                                            />
 
-                                        {errors.ngayphathanh && (
-                                            <p className="text-red-500 mt-1">{errors.ngayphathanh}</p>
-                                        )}
+                                            {errors.ngayphathanh && (
+                                                <p className="text-red-500 mt-1">{errors.ngayphathanh}</p>
+                                            )}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
 
-
-
-                        <div className="button-bottom flex justify-end">
-                            <button
-                                className="button-update text-amber-500 p-3 rounded-2xl mr-3 font-bold bg-white"
-                                onClick={() => {
-                                    setAccountNumber('');
-                                    setNgayphathanh(previousNgayphathanh); // Khôi phục giá trị trước đó
-                                    setSelectedBank('');
-                                    setErrors({});
-                                    setSuccessMessage('');
-                                }}
-                            >
-                                Hủy bỏ
-                            </button>
-                            <button className="button-update p-3 rounded-2xl bg-orange-500 font-bold text-white"
-                                    onClick={handleSubmit}>Cập nhật
-                            </button>
+                            <div className="button-bottom flex justify-end">
+                                <button
+                                    className="button-update text-amber-500 p-3 rounded-2xl mr-3 font-bold bg-white"
+                                    onClick={() => {
+                                        setAccountNumber('');
+                                        setNgayphathanh(previousNgayphathanh); // Khôi phục giá trị trước đó
+                                        setSelectedBank('');
+                                        setErrors({});
+                                        setSuccessMessage('');
+                                    }}
+                                >
+                                    Hủy bỏ
+                                </button>
+                                <button className="button-update p-3 rounded-2xl bg-orange-500 font-bold text-white"
+                                        onClick={handleSubmit}>Cập nhật
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
+                {successMessage && <p className="text-green-500">{successMessage}</p>}
             </div>
-            {successMessage && <p className="text-green-500">{successMessage}</p>}
-        </div>
         </div>
     );
 };
