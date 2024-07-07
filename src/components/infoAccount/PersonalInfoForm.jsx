94c0d109331889190e1def38019c0b8453fdb358
Tuan980Blue <166797678+Tuan980Blue@users.noreply.github.com>
2024-07-07 13:22:36 +0700

    Payment QRCode Tuan Modal (#34)
    * Select BANK tuan model
    
    * FIX repointsive BANK tuan model
    
    * FIX repointsive BANK tuan model
    
    * BANK QRCode tuan model

--- src/components/infoAccount/PersonalInfoForm.jsx
+++ src/components/infoAccount/PersonalInfoForm.jsx
@@ -4,55 +4,51 @@ import { HiMiniCheckBadge } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
 
 function MyWallet() {
-  return (
-    <div className="p-4 border-2 border-gray-300 rounded-2xl">
-      <div>
-        <div className="text-zinc-900 font-medium text-xl mb-2">VÍ CỦA TÔI</div>
-        <div className="flex flex-wrap">
-          <div className={"p-2 me-5"}>
-            <p className="text-sm text-gray-600">Số dư tổng</p>
-            <p className="text-lg text-rose-600 font-semibold">
-              {formattedAmount}
-            </p>
-          </div>
-          <div className={"p-2 me-5 "}>
-            <p className="text-sm text-gray-600">Số dư khả dụng</p>
-            <p className="text-lg font-semibold text-rose-600">0 đ</p>
-          </div>
-          <div className={"p-2 me-5 "}>
-            <p className="text-sm text-gray-600">Số dư đóng băng</p>
-            <p className="text-lg font-semibold text-rose-600">
-              {formattedAmount}
-            </p>
-          </div>
-          <div className={"p-2 me-5 "}>
-            <p className="text-sm text-gray-600">Số dư chờ chuyển</p>
-            <p className="text-lg font-semibold text-rose-600">0 đ</p>
-          </div>
+    return (
+        <div className="p-4 border-2 border-gray-200 rounded-2xl bg-white">
+            <div>
+                <div className="text-zinc-900 font-medium text-xl mb-2">VÍ CỦA TÔI</div>
+                <div className="flex flex-wrap">
+                    <div className={"p-2 me-5"}>
+                        <p className="text-sm text-gray-600">Số dư tổng</p>
+                        <p className="text-lg text-rose-600 font-semibold">{formattedAmount}</p>
+                    </div>
+                    <div className={"p-2 me-5 "}>
+                        <p className="text-sm text-gray-600">Số dư khả dụng</p>
+                        <p className="text-lg font-semibold text-rose-600">0 đ</p>
+                    </div>
+                    <div className={"p-2 me-5 "}>
+                        <p className="text-sm text-gray-600">Số dư đóng băng</p>
+                        <p className="text-lg font-semibold text-rose-600">{formattedAmount}</p>
+                    </div>
+                    <div className={"p-2 me-5 "}>
+                        <p className="text-sm text-gray-600">Số dư chờ chuyển</p>
+                        <p className="text-lg font-semibold text-rose-600">0 đ</p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
 
 function TopUpBtn() {
   const navigate = useNavigate();
 
-  const handleTopup = (e) => {
-    e.preventDefault();
-    // Logic xác thực người dùng ở đây
-    navigate("/topup");
-  };
-  return (
-    <div className="text-center" onClick={handleTopup}>
-      <button
-        className="bg-cyan-950 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline
-                    transition duration-300 ease-in-out hover:bg-blue-700 hover:text-white"
-      >
-        Nạp tiền vào ví
-      </button>
-    </div>
-  );
+    const handleTopup = (e) => {
+        e.preventDefault();
+        // Logic xác thực người dùng ở đây
+        navigate('/topup');
+    };
+    return (
+        <div className={"p-5"}>
+            <div className="text-center" onClick={handleTopup}>
+                <button className="bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline
+                    transition duration-300 ease-in-out hover:bg-green-800 hover:text-white">
+                    Nạp tiền vào ví
+                </button>
+            </div>
+        </div>
+    )
 }
 
 function formatCurrency(amount) {
