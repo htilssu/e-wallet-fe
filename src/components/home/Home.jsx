af552d9d4deace4fa5b72e06e913f27afbc3d7f0
htilssu <tolashuu@gmail.com>
2024-07-19 02:15:18 +0700

    fix: home layout response

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,72 +1,75 @@
-import {BsFillMenuButtonWideFill} from "react-icons/bs";
-import {Link, ScrollRestoration} from "react-router-dom";
+import { BsFillMenuButtonWideFill } from "react-icons/bs";
+import { Link, ScrollRestoration } from "react-router-dom";
 import PersonalInfoForm from "../infoAccount/PersonalInfoForm.jsx";
 import navbarItems from "./navbarItems.jsx";
 import ScrollableCardList from "./ScrollableCardList.jsx";
 import Slider from "../library component/Slider.jsx";
 
-const Card = ({icon, title, onClick}) => {
-    return (
-        <div
-            className="bg-white rounded-lg shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-300 cursor-pointer"
-            onClick={onClick}
-        >
-            <div className="p-8 flex flex-col items-center">
-                {icon}
-                <h2 className="text-xl font-semibold mt-4">{title}</h2>
-            </div>
-        </div>
-    );
+const Card = ({ icon, title, onClick }) => {
+  return (
+    <div
+      className="bg-white rounded-lg shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-300 cursor-pointer"
+      onClick={onClick}
+    >
+      <div className="p-8 flex flex-col items-center">
+        {icon}
+        <h2 className="text-xl font-semibold mt-4">{title}</h2>
+      </div>
+    </div>
+  );
 };
 
 const HomePage = () => {
-
-    return (
-        <div className={"flex flex-col bg-gray-100"}>
-            <div className={"flex justify-center items-center "}>
-                <div className={"flex flex-col md-[1000px]:flex-row"}>
-                    <div className={"order-first md:order-last md:ml-9"}>
-                        <PersonalInfoForm/>
-                    </div>
-                    <div className="min-h-screen flex flex-col md:ml-9 max-w-3xl">
-                        <div className="flex items-center justify-between p-4 w-full bg-white">
-                            <div className="flex items-center">
-                                <BsFillMenuButtonWideFill size={25} className="text-gray-800"/>
-                                <h1 className="text-2xl font-bold ml-3">Tiện ích</h1>
-                            </div>
-                        </div>
-                        <div className="flex-1 p-4">
-                            <div className="max-w-5xl mx-auto">
-                                <div className="grid grid-cols-2 sm:grid-cols-3 gap-6">
-                                    {navbarItems.map((item, index) => (
-                                        <Link to={item.link} key={index} className={"hover:no-underline"}>
-                                            <Card icon={item.icon} title={item.title}/>
-                                        </Link>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                        <div className={"flex flex-col p-4 mt-9 sm:mt-2"}>
-                            <div className={""}>
-                                <ScrollableCardList/>
-                            </div>
-                        </div>
-                        <div className={"w-full flex flex-col p-4 mb-28 sm:mt-2"}>
-                            <div className={"flex flex-col justify-center "}>
-                                <div className={"rs-text-medium text-lg"}>
-                                    Có thể bạn quan tâm
-                                </div>
-                                <div>
-                                    <Slider/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className={"flex flex-col bg-gray-100 px-10"}>
+      <div className={"flex w-full justify-center items-center "}>
+        <div className={"flex w-full flex-col md:flex-row"}>
+          <div className={"order-first md:order-last md:ml-9"}>
+            <PersonalInfoForm />
+          </div>
+          <div className="min-h-screen flex flex-col max-w-3xl">
+            <div className="flex items-center justify-between p-4 w-full bg-white">
+              <div className="flex items-center">
+                <BsFillMenuButtonWideFill size={25} className="text-gray-800" />
+                <h1 className="text-2xl font-bold ml-3">Tiện ích</h1>
+              </div>
             </div>
-            <ScrollRestoration/>
+            <div className="flex-1 p-4">
+              <div className="max-w-5xl mx-auto">
+                <div className="grid grid-cols-2 sm:grid-cols-3 gap-6">
+                  {navbarItems.map((item, index) => (
+                    <Link
+                      to={item.link}
+                      key={index}
+                      className={"hover:no-underline"}
+                    >
+                      <Card icon={item.icon} title={item.title} />
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className={"flex flex-col p-4 mt-9 sm:mt-2"}>
+              <div className={""}>
+                <ScrollableCardList />
+              </div>
+            </div>
+            <div className={"w-full flex flex-col p-4 mb-28 sm:mt-2"}>
+              <div className={"flex flex-col justify-center "}>
+                <div className={"rs-text-medium text-lg"}>
+                  Có thể bạn quan tâm
+                </div>
+                <div>
+                  <Slider />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+      <ScrollRestoration />
+    </div>
+  );
 };
 
 export default HomePage;
