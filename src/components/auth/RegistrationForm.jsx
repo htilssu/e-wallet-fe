1aa98c16ee53a7651da4610fca479fb32db1cacc
catchmeifyoucan99 <thanhphohochiminh5@gmail.com>
2024-07-31 06:34:34 +0700

    register

--- src/components/auth/RegistrationForm.jsx
+++ src/components/auth/RegistrationForm.jsx
@@ -1,69 +1,269 @@
-import { useEffect, useState } from "react";
+import "rsuite/dist/rsuite.min.css";
+import { MantineProvider, PasswordInput, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import dayjs from "dayjs";
-import { Button, PasswordInput, Stepper, TextInput } from "@mantine/core";
-import "@mantine/dates/styles.css";
-import customParseFormat from "dayjs/plugin/customParseFormat";
-import OTPverification from "../otpverify/OTPverification.jsx";
-import InputInformationForm from "./InputInformationForm.jsx";
-
-dayjs.extend(customParseFormat);
-
-dayjs("05/02/2024 01:02:03 PM -05:00", "DD/MM/YYYY HH:mm:ss A Z");
+import { post } from "../../util/requestUtil.js";
+import { useState } from "react";
 
 const RegistrationForm = ({ loginLink }) => {
-  const [steps, setSteps] = useState([
-    {
-      step: 1,
-      description: "Nhập thông tin",
-    },
-    {
-      step: 2,
-      description: "Xác thực email",
-    },
-  ]);
 
-  function onNextHandle() {
-    setCurrentStep(currentStep + 1);
+  const form = useForm({
+    initialValues: {
+      userName: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      repassword: "",
+      phoneNumber: "",
+      address: "",
+      city: "",
+      dob: "",
+      gender: "",
+      job: "",
+    },
+    validate: {
+      userName: (value) => (value ? null : "Vui lòng nhập tên đăng nhập"), // can be null
+      firstName: (value) => (value ? null : "Vui lòng nhập tên"),
+      lastName: (value) => (value ? null : "Vui lòng nhập họ"),
+      email: (value) => {
+        if (!value) return "Vui lòng nhập email";
+        if (!/\S+@\S+\.\S+/.test(value)) return "Email không hợp lệ";
+        return null;
+      },
+      password: (value) => (value ? null : "Yêu cầu nhập mật khẩu"),
+      repassword: (value, values) => (value === values.password ? null : "Mật khẩu không khớp"),
+      phoneNumber: (value) => {
+        if (value && (value.length < 0 || value.length > 10)) return "Số điện thoại phải có độ dài từ 0 đến 10 ký tự";
+        return null;
+      },
+      address: () => null, // can be null
+      city: () => null, // can be null
+      dob: (value) => (value ? null : "Vui lòng nhập ngày sinh"),
+      gender: (value) => (value ? null : "Vui lòng chọn giới tính"),
+      job: () => null, // can be null
+    },
+  });
+
+  const [error, setError] = useState(null);
+
+  async function handleRegister() {
+    form.validate();
+
+    if (form.isValid()) {
+      post("/api/v1/auth/register", {
+        userName: form.values.userName,
+        firstName: form.values.firstName,
+        lastName: form.values.lastName,
+        email: form.values.email,
+        password: form.values.password,
+        phoneNumber: form.values.phoneNumber,
+        address: form.values.address,
+        city: form.values.city,
+        dob: form.values.dob,
+        gender: form.values.gender,
+        job: form.values.job,
+      })
+        .then((res) => {
+          if (res.data.user) {
+            localStorage.setItem("user", JSON.stringify(res.data.user));
+            localStorage.setItem("token", res.data.token);
+            location.href = "/login";
+          } else {
+            setError(res.data.message);
+          }
+        })
+        .catch((res) => {
+          if (res.response && res.response.data.message) {
+            setError(res.response.data.message);
+          }
+        });
+    } else {
+      setError(null);
+    }
   }
 
-  const [currentStep, setCurrentStep] = useState(0);
-
   return (
-    <div className="form-box register md:p-4 h-0 flex items-center min-h-screen justify-center">
-      <div
-        className={
-          "w-full bg-white sm:bg-transparent h-full flex flex-col justify-center items-center"
-        }
-      >
-        <div className={"bg-white sm:w-7/12 rounded-xl  md:w-6/12 lg:w-4/12"}>
-          <div className="hidden md:flex p-4 flex-col">
-            <Stepper
-              styles={{
-                stepIcon: {
-                  backgroundColor: "white",
-                },
-                stepCompletedIcon: {
-                  backgroundColor: "#40c057",
-                  borderRadius: "100%",
-                },
+    <MantineProvider>
+      <div className="min-h-screen w-full p-4 overflow-hidden bg-gray-100 text-gray-900 flex justify-center">
+        <div className="w-full m-0 bg-white rounded-lg overflow-hidden shadow sm:rounded-lg flex justify-center flex-1">
+          <div className="flex-1 bg-green-100 text-center hidden sm:flex">
+            <div
+              className="w-full bg-contain bg-center bg-no-repeat"
+              style={{
+                backgroundImage: 'url("/login_banner.webp")',
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
               }}
-              color="green"
-              active={currentStep}
             >
-              {steps.map((step) => (
-                <Stepper.Step
-                  key={step.step}
-                  label={"Bước " + step.step}
-                  description={step.description}
-                />
-              ))}
-            </Stepper>
+              <div
+                className={"w-full h-full opacity-15"}
+                style={{
+                  backgroundImage: 'url("/noise.png")',
+                  backgroundSize: "cover",
+                  backgroundRepeat: "repeat-x",
+                }}
+              ></div>
+            </div>
           </div>
-          <div className={"p-4"}>{<InputInformationForm />}</div>
+          <div className="sm:w-3/5 relative z-10 w-full flex justify-center items-center md:w-1/2 lg:w-5/12 md:p-6 sm:p-12">
+            <div className="w-full flex flex-col justify-center items-center">
+              <div className={"text-2xl font-bold"}>Đăng Ký</div>
+              <div className="w-full px-8 md:px-8 lg:px-20 flex flex-col items-center mt-8">
+                <div className="w-full">
+                  <form onSubmit={form.onSubmit(handleRegister)}>
+                    <div>
+                      <TextInput
+                        size={"md"}
+                        key={form.key("userName")}
+                        placeholder={"User Name"}
+                        {...form.getInputProps("userName")}
+                      />
+                    </div>
+                    <div className="mt-3">
+                      <TextInput
+                        size={"md"}
+                        key={form.key("firstName")}
+                        placeholder={"Tên"}
+                        {...form.getInputProps("firstName")}
+                      />
+                    </div>
+                    <div className="mt-3">
+                      <TextInput
+                        size={"md"}
+                        key={form.key("lastName")}
+                        placeholder={"Họ"}
+                        {...form.getInputProps("lastName")}
+                      />
+                    </div>
+                    <div className="mt-3">
+                      <TextInput
+                        size={"md"}
+                        key={form.key("email")}
+                        placeholder={"Email"}
+                        {...form.getInputProps("email")}
+                      />
+                    </div>
+                    <div className="mt-3">
+                      <PasswordInput
+                        size={"md"}
+                        key={form.key("password")}
+                        placeholder={"Mật Khẩu"}
+                        {...form.getInputProps("password")}
+                      />
+                    </div>
+                    <div className="mt-3">
+                      <PasswordInput
+                        size={"md"}
+                        key={form.key("repassword")}
+                        placeholder={"Nhập lại mật khẩu"}
+                        {...form.getInputProps("repassword")}
+                      />
+                    </div>
+                    <div className="mt-3">
+                      <TextInput
+                        size={"md"}
+                        key={form.key("phoneNumber")}
+                        placeholder={"Số điện thoại"}
+                        {...form.getInputProps("phoneNumber")}
+                      />
+                      {form.errors.phoneNumber && (
+                        <p className="text-red-500 text-sm">{form.errors.phoneNumber}</p>
+                      )}
+                    </div>
+                    <div className="mt-3">
+                      <TextInput
+                        size={"md"}
+                        key={form.key("address")}
+                        placeholder={"Địa chỉ"}
+                        {...form.getInputProps("address")}
+                      />
+                    </div>
+                    <div className="mt-3">
+                      <TextInput
+                        size={"md"}
+                        key={form.key("city")}
+                        placeholder={"Thành phố"}
+                        {...form.getInputProps("city")}
+                      />
+                    </div>
+                    <div className="mt-3">
+                      <TextInput
+                        size={"md"}
+                        type="date"
+                        key={form.key("dob")}
+                        placeholder={"Ngày sinh"}
+                        {...form.getInputProps("dob")}
+                      />
+                    </div>
+                    <div className="mt-3">
+                      <select
+                        name="gender"
+                        value={form.values.gender}
+                        onChange={(event) =>
+                          form.setFieldValue("gender", event.currentTarget.value)
+                        }
+                        className="w-full p-2 border border-gray-300 rounded-sm"
+                      >
+                        <option value="true">Nam</option>
+                        <option value="false">Nữ</option>
+                      </select>
+                      {form.errors.gender && (
+                        <p className="text-red-500 text-sm">{form.errors.gender}</p>
+                      )}
+                    </div>
+                    <div className="mt-3">
+                      <TextInput
+                        size={"md"}
+                        key={form.key("job")}
+                        placeholder={"Nghề nghiệp"}
+                        {...form.getInputProps("job")}
+                      />
+                    </div>
+                    <div>
+                      {error && (
+                        <div className={"text-sm mt-1"}>
+                          <p className={"text-red-400"}>{error}</p>
+                        </div>
+                      )}
+                    </div>
+                    <div className="px-2">
+                      <button
+                        type={"submit"}
+                        className="mt-5 font-semibold bg-primary hover:bg-primaryHover text-white  border border-primary w-full py-2 rounded-lg hover:bg-primary hover:text-white transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                      >
+                        Đăng Ký
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <a
+                  href={loginLink ? loginLink : "/login"}
+                  className={"hover:no-underline hover:text-primary mt-2"}
+                >
+                  Đăng nhập
+                </a>
+              </div>
+              <div
+                className={"hidden w-full h-full opacity-15 z-100"}
+                style={{
+                  backgroundImage: 'url("/login_banner.webp")',
+                }}
+              >
+                <div
+                  className={"w-full h-full opacity-100 -z-1"}
+                  style={{
+                    backgroundImage: 'url("/noise.png")',
+                    backgroundSize: "cover",
+                    backgroundRepeat: "repeat-x",
+                  }}
+                ></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </MantineProvider>
   );
 };
 
